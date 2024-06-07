b1f2b42b1fd3a612871eb1193cc6ed2ddffa70bd
MilicentAoko <milicentaoko5@gmail.com>
2024-06-07 11:38:53 +0300

    Add files via upload

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL ='https://forkify-api.herokuapp.com/api/v2/recipes'
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'f2458453-209b-4721-8aef-832eb14b6be2';
+export const MODAL_CLOSE_SEC = 2.5;
